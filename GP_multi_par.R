2a76e601898ac282a8d3f170bebc622ede488afb
KuanJiangJK <k.jiang@tilburguniversity.edu>
2025-05-01 09:11:03 +0200

    GP regression for multiple variates.

---
+++ GP_multi_par.R
@@ -0,0 +1,87 @@
+N_obs <- 30
+K <- 4 # number of parameters (including the 1's colum for the intercept)
+x11 <- rnorm(N_obs) # the first index 1 means observation data, thus 2 means testing data point
+x12 <- rnorm(N_obs)
+x13 <- runif(N_obs, 0, 10)
+X1 <- matrix(data = c(rep(1,N_obs), x11, x12, x13), nrow = N_obs, ncol = K)
+Y1 <- 2+  0.5 * x11 - 0.4 * x12 + 2* sin(x13)  + rnorm(N_obs, 0,1)
+
+
+N_test <- 100
+x21 <- rnorm(N_test)
+x22 <- rnorm(N_test)
+x23 <- runif(N_test, 0, 10)
+X2 <- matrix(data = c(rep(1,N_test), x21, x22, x23), nrow = N_test, ncol = K)
+
+stan.data <- list(N_obs= N_obs,
+                  K = K,
+                  X1 = X1,
+                  Y1 = Y1,
+                  N_test = N_test ,
+                  X2 = X2)
+
+sample_STAN <- stan(file = 'GP_multi_par.stan',
+                     data = stan.data,
+                     chains = 2, 
+                     warmup = 1e3,
+                     iter = 3e3)
+# Chain 1: Gradient evaluation took 0.005385 seconds
+# Chain 1: 1000 transitions using 10 leapfrog steps per transition would take 53.85 seconds.
+# Chain 1: Adjust your expectations accordingly!
+# Chain 1:  Elapsed Time: 303.453 seconds (Warm-up)
+# Chain 1:                585.11 seconds (Sampling)
+# Chain 1:                888.563 seconds (Total)
+# Chain 2: Gradient evaluation took 0.004339 seconds
+# Chain 2: 1000 transitions using 10 leapfrog steps per transition would take 43.39 seconds.
+# Chain 2: Adjust your expectations accordingly!
+# Chain 2:  Elapsed Time: 345.672 seconds (Warm-up)
+# Chain 2:                603.312 seconds (Sampling)
+# Chain 2:                948.984 seconds (Total)
+# Warning messages:
+# 1: There were 30 divergent transitions after warmup. See
+# https://mc-stan.org/misc/warnings.html#divergent-transitions-after-warmup
+# to find out why this is a problem and how to eliminate them. 
+# 2: Examine the pairs() plot to diagnose sampling problems
+
+
+plot(sample_STAN, pars = "beta", plotfun = "stan_trace", nrow = 4)
+plot(sample_STAN, pars = "rec_L2", plotfun = "stan_trace", nrow = 3)
+plot(sample_STAN, pars = "sigman", plotfun = "stan_trace", nrow = 1)
+plot(sample_STAN, pars = c("sigmaf", "sigmaf2"), plotfun = "stan_trace", nrow = 2)
+
+posterior_samples <- rstan::extract(sample_STAN)
+beta_draws <- (posterior_samples$beta)
+rec_L2_draws <- (posterior_samples$rec_L2)
+sigmaf2_draws <- (posterior_samples$sigmaf2)
+sigman_draws <- (posterior_samples$sigman)
+
+par(mfrow=c(4,1))
+plot(density(beta_draws[,1]))
+abline(v = mean(beta_draws[,1]),col = "red")
+plot(density(beta_draws[,2]))
+abline(v = mean(beta_draws[,2]),col = "red")
+plot(density(beta_draws[,3]))
+abline(v = mean(beta_draws[,3]),col = "red")
+plot(density(beta_draws[,4]))
+abline(v = mean(beta_draws[,4]),col = "red")
+
+par(mfrow=c(3,1))
+plot(density(rec_L2_draws[,1]))
+abline(v = mean(rec_L2_draws[,1]),col = "red")
+plot(density(rec_L2_draws[,2]))
+abline(v = mean(rec_L2_draws[,2]),col = "red")
+plot(density(rec_L2_draws[,3]))
+abline(v = mean(rec_L2_draws[,3]),col = "red")
+
+par(mfrow=c(2,1))
+plot(density(sigmaf2_draws))
+abline(v = mean(sigmaf2_draws),col = "red")
+plot(density(sigman_draws))
+abline(v = mean(sigman_draws),col = "red")
+
+alpha2_draws_2 <- posterior_samples_62$alpha2
+plot(density(alpha2_draws_1), xlim = c(0,2), ylim = c(0, 10) )
+lines(density(alpha2_draws_2), xlim = c(0,2), col=2)
+plot(alpha2_draws_1, ylim = c(0,10))
+plot(alpha2_draws_2, ylim = c(0,10))
+
---
+++ GP_multi_par.stan
@@ -0,0 +1,65 @@
+data {
+  int<lower=1> N_obs;
+  int<lower=0> K; // number of predictors (including intercept, corresponding to 1's in X)
+  matrix[N_obs, K] X1;
+  vector[N_obs] Y1; // training point
+  int<lower=1> N_test;
+  matrix[N_test, K] X2; // testing point
+}
+
+transformed data {
+  real delta = 1e-9;  // jitter to stabilize the cholesky_decompose
+  int<lower=1> N = N_obs + N_test;
+  matrix[N, K] X; //combine observation and test points
+  for (i in 1:N_obs) {
+    X[i,] = X1[i,];
+  }
+  for (j in 1:N_test) {
+    X[N_obs + j,] = X2[j,];
+  }
+  matrix[N, K-1] XX = block(X, 1, 2, N, K-1); // here drop the 1st column (all 1's), might be helpful for calculating distance between two points
+}
+
+parameters {
+  vector[K] beta;
+  real<lower=0> sigmaf; // magnitude of the GP, the sigma2 before the exp...
+  vector<lower=0>[K-1] rec_L2; // reciprocal of the square length scale. 1/(L^2)
+  real<lower=0> sigman; // standard deviation of measurement error
+  vector[N] eta; // this, together with L_kernel (cholesky decomposition), is a substitute of the sampled function f, making sampling more efficient. f = Lk*eta. When L_kernel is determined given parameters (sigmaf & length scale), eta~N(0,1) is the randomness lefted for a random function.
+}
+
+transformed parameters {
+  matrix[K-1, K-1] M = diag_matrix(rec_L2); // each dimension has its own "length scale".
+  vector[N] f;  // value of GP(0, KERNEL)
+  vector[N] F;  // value of total function, Beta*X + GP = Yi - e
+  matrix[N, N] Kernel;
+  matrix[N, N] L_Kernel;  // decomposition
+  real<lower=0> sigmaf2 = square(sigmaf);
+  
+  // Covariance matrix with squared exponential kernel
+  for (i in 1:(N - 1)) {
+    Kernel[i, i] = sigmaf2 + delta;
+    for (j in (i + 1):N) {
+      vector[K-1] distance = to_vector(XX[i,] - XX[j,]);
+      Kernel[i, j] = sigmaf2* exp(-0.5 * distance' * M * distance);
+      Kernel[j, i] = Kernel[i, j];
+    }
+  }
+  Kernel[N, N] = sigmaf2 + delta;
+  L_Kernel = cholesky_decompose(Kernel);
+  f = L_Kernel * eta;
+  F = f + X*beta;
+  }
+
+model {
+  rec_L2 ~ multi_normal(rep_vector(0, K-1), diag_matrix(rep_vector(1, K-1)));
+  beta ~ multi_normal(rep_vector(0, K), diag_matrix(rep_vector(1, K)));
+  sigmaf ~ student_t(1, 0, 1);
+  eta ~ std_normal();
+  sigman ~ normal(0, 1);
+  Y1 ~ normal(F[1:N_obs], sigman); // this is the likelihood of data.
+}
+
+
+
+
