be976d5a9344f9f35ea3d2a100d463f724ee5bad
KuanJiangJK <k.jiang@tilburguniversity.edu>
2025-05-15 17:33:17 +0200

    GP ANCOVA with shrinkage prior. Works well... Comparsion made in GP_ANCOVA.R.....

--- GP_ANCOVA.R
+++ GP_ANCOVA.R
@@ -1,14 +1,19 @@
+## number of test points affect sampling speed. 
+# it also affects the plotting function, change the number accodingly.  
+# when preparing data for plot, I have ####sample_f = rep(seq(1,10),each = 200)#### this works when N_test = 200. 
+## change it accordingly when change N_test
 rm(list = ls())
 ## GP_ANCOVA
 
 library(MASS)     # for mvrnorm
 library(rstan)
+library(ggplot2)
 
 set.seed(123)
 
 # Parameters
 N_obs <- 70
-N_test <- 200
+N_test <- 100
 K <- 1            # number of covariates
 J <- 3             # number of groups
 
@@ -62,6 +67,7 @@ stan_data <- list(
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
+######### Session for GP_ANOCVA.stan (without horseshoe prior) ####################
 fit <- stan(
   file = "GP_ANCOVA.stan",
   data = stan_data,
@@ -69,6 +75,7 @@ fit <- stan(
   chains = 1,
   seed = 123
 )
+
 
 library(rstan)
 library(ggplot2)
@@ -105,7 +112,7 @@ mean_df <- data.frame(x = X2,
 plot.df <- data.frame(x = rep(X2, times= 10),
                       y = as.vector(t(f_test_samples[draws,])),
                       group = factor(group2),
-                      sample_f = rep(seq(1,10),each = 200))
+                      sample_f = rep(seq(1,10),each = 100))
 
 
 
@@ -118,3 +125,81 @@ ggplot() +
        x = "X", y = "f(x)") +
   theme_minimal() +
   scale_color_brewer(palette = "Dark2")
+
+######### Session for GP_ANOCVA_2.stan (with horseshoe prior) ####################
+fit2 <- stan(
+  file = "GP_ANCOVA_2.stan",
+  data = stan_data,
+  iter = 2000,
+  chains = 1,
+  seed = 123
+)
+
+post2 <- rstan::extract(fit2)
+posterior_df2 <- as.data.frame(fit2)
+
+# Parameters to trace
+par(mfrow=c(2,3))
+plot(fit2, pars = "alpha", plotfun = "stan_trace")
+plot(fit2, pars = "rho", plotfun = "stan_trace")
+plot(fit2, pars = "sigma", plotfun = "stan_trace")
+plot(fit2, pars = "beta", plotfun = "stan_trace")
+plot(fit2, pars = "mu", plotfun = "stan_trace")
+par(mfrow=c(1,1))
+plot(density(post2$alpha))
+abline(v = mean(post2$alpha), col = "red")
+abline(v = median(post2$alpha), col = "blue")
+
+# observed data
+obs_df <- data.frame(
+  x = X1,
+  y = Y1,
+  group = as.factor(group1)
+)
+# test inputs
+N_test <- nrow(X2)
+f_samples <- post2$f_group  # dimension: iterations x N_obs + N_test
+# Extract only test part
+f_test_samples <- f_samples[, (nrow(X1) + 1):(nrow(X1) + N_test)]
+# sampling 10 per group
+draws <- sample(1e3, 10)
+
+mean_df <- data.frame(x = X2,
+                      y = colMeans(f_test_samples),
+                      group = factor(group2))
+
+
+plot.df <- data.frame(x = rep(X2, times= 10),
+                      y = as.vector(t(f_test_samples[draws,])),
+                      group = factor(group2),
+                      sample_f = rep(seq(1,10),each = 100))
+
+
+
+# Plot everything
+ggplot() +
+  geom_point(data = obs_df, aes(x = x, y = y, color = group), size = 1.5) +
+  geom_line(data = mean_df, aes(x = x, y = y, color = group), linewidth = 1.2) + 
+  geom_line(data = plot.df, aes(x = x, y = y, group = interaction(sample_f, group), color = group), linewidth = 0.5, alpha = 0.5) +
+  labs(title = "Observed Data and Posterior Samples by Group",
+       x = "X", y = "f(x)") +
+  theme_minimal() +
+  scale_color_brewer(palette = "Dark2")
+
+
+## Compare the two files ----
+post <- rstan::extract(fit)
+posterior_df <- as.data.frame(fit)
+post2 <- rstan::extract(fit2)
+posterior_df2 <- as.data.frame(fit2)
+
+par(mfrow=c(2,2))
+plot(density(post$alpha))
+abline(v = mean(post$alpha), col = "red")
+abline(v = median(post$alpha), col = "blue")
+plot(density(post2$alpha))
+abline(v = mean(post2$alpha), col = "red")
+abline(v = median(post2$alpha), col = "blue")
+
+## Rdata saved as > save.image("~/Documents/R/gaussian_process/GP_ANCOVA_output.RData")
+
--- GP_ANCOVA_2.stan
+++ GP_ANCOVA_2.stan
@@ -1,5 +1,3 @@
-// This version of ANCOVA. Yij = f(x) + muj + eij, where I constrained Sum-up of muj to be 0, because of the identifiability issues
-// Without constraint, the effect of group might be shared by the general non-linear function f(x)
 // More interesting one should be Yij = f(x) + muj + eij. where a shrinkage prior being used in the non-linear effet, indicating we want to see a linear effect rather than non-linear,
 // while still retain some sensitivity to non-linear trend.
 // But what should I do to make it identifiable? (see next one GP_ANCOVA_2.Stan)
@@ -35,10 +33,12 @@ transformed data{
   }
 }
 
+
 parameters {
   real<lower=0> alpha;        // GP marginal std
   real<lower=0> rho;          // GP length-scale
   real<lower=0> sigma;        // observation noise
+  vector[K] beta;               // linear effect coefs.
   vector[J] mu;               // group effect
   vector[N] eta;            // latent GP at all inputs
 }
@@ -53,54 +53,26 @@ transformed parameters {
   
   matrix[N, N] L_Kernel;  // decomposition
   L_Kernel = cholesky_decompose(K_f);
-  f = L_Kernel * eta;
-  f = f - mean(f);
+  f = L_Kernel * eta; // this is f(x)
+  f = f - mean(f); // constrain f(x) to make it identifiable, or it would meddle with muj
   vector[N] f_group;
-  f_group = mu[group] + f; // this is the function value with group effect.
+  vector[N] xbeta;
+  for(i in 1: N){
+    xbeta[i] = dot_product(X[i], beta);
+  }
+  f_group = mu[group] + f + xbeta ; // this is the function value with group effect.
 }
 
 model {
   // Priors
-  alpha ~ normal(0, 5);
+  alpha ~ student_t(1, 0, 1);
   rho ~ inv_gamma(5, 5);
   sigma ~ cauchy(0, 0.1);
   mu ~ normal(0, 2);
   eta ~ normal(0,1);
+  beta ~ normal(0,1);
   // Likelihood
   Y1 ~ normal(f_group[1:N_obs], sigma);
 }
-
-
-// generated quantities {
-//   vector[N_test] f_test;
-//   vector[N_test] y_test_pred;
-// 
-//   {
-//     matrix[N_obs, N_obs] K = K_f;
-//     matrix[N_test, N_obs] K_s;
-//     matrix[N_test, N_test] K_ss;
-// 
-//     for (i in 1:N_test) {
-//       for (j in 1:N_obs) {
-//         real sq_dist = squared_distance(X2[i], X1[j]);
-//         K_s[i, j] = alpha^2 * exp(-0.5 * sq_dist / rho^2);
-//       }
-//     }
-// 
-//     for (i in 1:N_test) {
-//       for (j in 1:N_test) {
-//         real sq_dist = squared_distance(X2[i], X2[j]);
-//         K_ss[i, j] = alpha^2 * exp(-0.5 * sq_dist / rho^2);
-//       }
-//       K_ss[i, i] += 1e-6; // jitter
-//     }
-// 
-//     matrix[N_test, N_obs] K_s_Kinv = K_s * inverse(K);
-//     f_test = K_s_Kinv * f;
-//     for (i in 1:N_test) {
-//       y_test_pred[i] = normal_rng(f_test[i] + mu[group2[i]], sigma);
-//     }
-//   }
-// }
 
 
