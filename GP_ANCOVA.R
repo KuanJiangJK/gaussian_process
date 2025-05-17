129f49f13722df2b7db4fe51e6730eeaedd6e71b
KuanJiangJK <k.jiang@tilburguniversity.edu>
2025-05-17 02:41:08 +0200

    New model finished. Works fine but struggling to address divergent transition issues (mostly 20 per 2000 iterations). running time is acceptable. 170 sample size within 6 mins.

--- GP_ANCOVA.R
+++ GP_ANCOVA.R
@@ -186,6 +186,69 @@ ggplot() +
   theme_minimal() +
   scale_color_brewer(palette = "Dark2")
 
+######### Session for GP_ANOCVA_3.stan (with horseshoe prior) ####################
+fit3 <- stan(
+  file = "GP_ANCOVA_3.stan",
+  data = stan_data,
+  iter = 2000,
+  chains = 1,
+  seed = 123
+)
+
+post3 <- rstan::extract(fit3)
+posterior_df3 <- as.data.frame(fit3)
+
+# Parameters to trace
+par(mfrow=c(2,3))
+plot(fit3, pars = "alpha", plotfun = "stan_trace")
+plot(fit3, pars = "rho", plotfun = "stan_trace")
+plot(fit3, pars = "sigma", plotfun = "stan_trace")
+plot(fit3, pars = "beta", plotfun = "stan_trace")
+plot(fit3, pars = "mu", plotfun = "stan_trace")
+plot(fit3, pars = "g_alpha", plotfun = "stan_trace")
+plot(fit3, pars = "g_rho", plotfun = "stan_trace")
+par(mfrow=c(1,1))
+plot(density(post3$alpha))
+abline(v = mean(post3$alpha), col = "red")
+abline(v = median(post3$alpha), col = "blue")
+
+# observed data
+obs_df <- data.frame(
+  x = X1,
+  y = Y1,
+  group = as.factor(group1)
+)
+# test inputs
+N_test <- nrow(X2)
+f_samples <- post3$F  # dimension: iterations x N_obs + N_test
+# Extract only test part
+f_test_samples <- f_samples[, (nrow(X1) + 1):(nrow(X1) + N_test)]
+# sampling 10 per group
+draws <- sample(1e3, 10)
+
+mean_df <- data.frame(x = X2,
+                      y = colMeans(f_test_samples),
+                      group = factor(group2))
+
+
+plot.df <- data.frame(x = rep(X2, times= 10),
+                      y = as.vector(t(f_test_samples[draws,])),
+                      group = factor(group2),
+                      sample_f = rep(seq(1,10),each = 100))
+
+
+
+# Plot everything
+ggplot() +
+  geom_point(data = obs_df, aes(x = x, y = y, color = group), size = 1.5) +
+  geom_line(data = mean_df, aes(x = x, y = y, color = group), linewidth = 1.2) + 
+  geom_line(data = plot.df, aes(x = x, y = y, group = interaction(sample_f, group), color = group), linewidth = 0.5, alpha = 0.5) +
+  labs(title = "Observed Data and Posterior Samples by Group",
+       x = "X", y = "f(x)") +
+  theme_minimal() +
+  scale_color_brewer(palette = "Dark2")
+
+
 
 ## Compare the two files ----
 post <- rstan::extract(fit)
@@ -201,5 +264,4 @@ plot(density(post2$alpha))
 abline(v = mean(post2$alpha), col = "red")
 abline(v = median(post2$alpha), col = "blue")
 
-## Rdata saved as > save.image("~/Documents/R/gaussian_process/GP_ANCOVA_output.RData")
 
