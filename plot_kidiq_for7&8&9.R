3c851aacba3a02e3c703e254a290badd3a33950a
KuanJiangJK <k.jiang@tilburguniversity.edu>
2025-06-25 19:01:56 +0200

    Add user-defined regularization (see GP_ANCOVA_9).

--- plot_kidiq_for7&8&9.R
+++ plot_kidiq_for7&8&9.R
@@ -1,6 +1,5 @@
 ##################### PLOT PLOT PLOT Functions ###############
 # Plots for alpha/sigma/length_scale
-
 plot_para <- function(object, col = "black", breaks = 1e2, title, minlim = min(object, na.rm =TRUE), maxlim = max(object, na.rm =TRUE)){
   par(mar=c(2, 2, 1.5, 0.5))
   if(is.na(ncol(object))){
@@ -36,6 +35,7 @@ std_col_stan <- function(object, cols = seq(1:ncol(object))){
   }
   return(object)
 }
+
 # data
 kidiq <- read.csv("kidiq.csv")
 kidiq <- std_col_stan(kidiq, cols = c(1,3,4))
@@ -54,6 +54,7 @@ X2_momage <- seq(from = min(kidiq$mom_age), to = max(kidiq$mom_age), length.out 
 X2 <- as.matrix(cbind(X2_momiq, X2_momage))
 colnames(X2) <- NULL
 
+# Prepare data for rstan. Without c^2 regularization
 kidiq_stan <- list(
   N1 = N_obs,
   N2 = N_test,
@@ -82,17 +83,121 @@ kidiq_stan_fit_8 <- stan(
   seed = 123
 )
 
+# Prepare user-defined regularization, by setting different c^2, you get different cap for the alpha2
+# c^2 should be determined relative to the magnitude of tau^2*lambda^2 (according to the paper)
+# where by defination, lambda ~ half-Cauchy(0, 1), truncated to larger than 0.
+# Here WE don't have tau^2, which is global shrinkage parameter, so assumed to be just 1.
+# the quantile values of Cauchy^2 might be helpful for determining a valid value for c^2
+sample_cauchy2 <- abs(rcauchy(1e5, 0,1))^2
+quantile(sample_cauchy2, probs = c(0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99))
+#         1%          10%          25%          50%          75%          90%          95%          99% 
+#   2.488663e-04 2.656248e-02 1.740023e-01 1.005081e+00 5.806097e+00 3.942509e+01 1.559432e+02 3.761669e+03 
+
+### With tunned c2 for regularized horseshoe prior
+kidiq_stan_5 <- list(  # Cap top 5% value of the alpha2 (I am not sure if it really did so. Need verify)
+  N1 = N_obs,
+  N2 = N_test,
+  K = K,
+  J = J,
+  X1 = X1,
+  X2 = X2,
+  Y1 = Y1,
+  group1 = group1,
+  group2 = group2,
+  c_global2 =  160,
+  c_group2 = 160
+)
+
+kidiq_stan_25 <- list( # Cap top 25% value of the alpha2
+  N1 = N_obs,
+  N2 = N_test,
+  K = K,
+  J = J,
+  X1 = X1,
+  X2 = X2,
+  Y1 = Y1,
+  group1 = group1,
+  group2 = group2,
+  c_global2 =  5,
+  c_group2 = 5
+)
+
+kidiq_stan_50 <- list( # Cap top 50% value of the alpha2
+  N1 = N_obs,
+  N2 = N_test,
+  K = K,
+  J = J,
+  X1 = X1,
+  X2 = X2,
+  Y1 = Y1,
+  group1 = group1,
+  group2 = group2,
+  c_global2 =  1,
+  c_group2 = 1
+)
+
+kidiq_stan_75 <- list( # Cap top 75% value of the alpha2
+  N1 = N_obs,
+  N2 = N_test,
+  K = K,
+  J = J,
+  X1 = X1,
+  X2 = X2,
+  Y1 = Y1,
+  group1 = group1,
+  group2 = group2,
+  c_global2 =  0.17,
+  c_group2 = 0.17
+)
+
+kidiq_stan_fit_5 <- stan(
+  file = "GP_ANCOVA_9.stan",
+  data = kidiq_stan_5,
+  iter = 2000,
+  chains = 1,
+  seed = 123
+)
+kidiq_stan_fit_25 <- stan(
+  file = "GP_ANCOVA_9.stan",
+  data = kidiq_stan_25,
+  iter = 2000,
+  chains = 1,
+  seed = 123
+)
+kidiq_stan_fit_50 <- stan(
+  file = "GP_ANCOVA_9.stan",
+  data = kidiq_stan_50,
+  iter = 2000,
+  chains = 1,
+  seed = 123
+)
+kidiq_stan_fit_75 <- stan(
+  file = "GP_ANCOVA_9.stan",
+  data = kidiq_stan_75,
+  iter = 2000,
+  chains = 1,
+  seed = 123
+)
+
+
 ## PLOT kidiq #####
+
+# Extract stan fit
 post_kid <- rstan::extract(kidiq_stan_fit_7)
 post_kid <- rstan::extract(kidiq_stan_fit_8)
 
-# plot_para(post_kid$lambda_global^2, breaks = 1e4, title = "lambda_global^2", maxlim = 1) # for GP_ANCOVA_8 only
-# plot_para(post_kid$lambda_group^2, breaks = 1e4, title = "lambda_group^2", maxlim = 1) # for GP_ANCOVA_8 only
+post_kid <- rstan::extract(kidiq_stan_fit_5) # cap top 5%
+post_kid <- rstan::extract(kidiq_stan_fit_25) # cap top 25%
+post_kid <- rstan::extract(kidiq_stan_fit_50) # cap top 50%
+post_kid <- rstan::extract(kidiq_stan_fit_75) # cap top 75%
+
+# plot_para(post_kid$lambda_global^2, breaks = 1e4, title = "lambda_global^2", maxlim = 1) # for GP_ANCOVA_8 only, where c is free to estimated
+# plot_para(post_kid$lambda_group^2, breaks = 1e4, title = "lambda_group^2", maxlim = 1) # for GP_ANCOVA_8 only, where c is free to estimated
 plot_para(post_kid$sigma^2, breaks = 1e2, title = "sigma^2")
-plot_para(post_kid$alpha^2, breaks = 3e2, title = "alpha^2", maxlim = 1)
+plot_para(post_kid$alpha^2, breaks = 3e2, title = "alpha^2")
 plot_para(post_kid$rho, breaks = 1e2, title = "rho")
 plot_para(post_kid$mu_j, title = "mu_j group")
-plot_para(post_kid$g_alpha^2, breaks = 1e2, title = "alpha^2 group", maxlim = 1.5)
+plot_para(post_kid$g_alpha^2, breaks = 1e2, title = "alpha^2 group", maxlim = 1.51)
 plot_para(post_kid$g_rho^2, breaks = 1e2, title = "rho^2 group", maxlim = 20)
 
 plot(kidiq_stan_fit_7, pars = "alpha", plotfun = "stan_trace")
@@ -108,6 +213,7 @@ plot(kidiq_stan_fit_8, pars = "rho", plotfun = "stan_trace")
 plot(kidiq_stan_fit_8, pars = "g_rho", plotfun = "stan_trace", nrow = 3)
 plot(kidiq_stan_fit_8, pars = "mu_j", plotfun = "stan_trace", nrow = 3)
 plot(kidiq_stan_fit_8, pars = "sigma", plotfun = "stan_trace", nrow = 3) 
+
 ## training point for plot
 obs_df <- data.frame(
   x = kidiq_stan$X1,
